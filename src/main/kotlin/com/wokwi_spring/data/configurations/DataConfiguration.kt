c982c73c46295b434c75a2040bd294509b6adf4f
vladyslav.sievostianov <vladyslav.sievostianov@stfalcon.com>
2024-10-24 15:01:15 +0300

    Remove unnecessary mongo configuration. Enable CORS.

--- src/main/kotlin/com/wokwi_spring/data/configurations/DataConfiguration.kt
+++ src/main/kotlin/com/wokwi_spring/data/configurations/DataConfiguration.kt
@@ -6,6 +6,6 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 
 @Configuration
-@Import(MongoConfiguration::class, MqttConfiguration::class)
+@Import(MqttConfiguration::class)
 @ComponentScan(basePackageClasses = [SensorRepositoryImpl::class])
 class DataConfiguration
